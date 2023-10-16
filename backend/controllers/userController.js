2232e69312861da34b4be913e68fbf9f6cdc7bb3
FahadKabirKsl <fahadkabirsezan@gmail.com>
2023-10-16 09:53:53 +0600

    learn

---
+++ backend/controllers/userController.js
@@ -0,0 +1,78 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
+// @desc Auth user/set token
+// route POST /api/users/auth
+// access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (user && (await user.matchPassword(password))) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+// @desc register new user
+// route POST /api/users
+// access Public
+const registerUser = asyncHandler(async (req, res) => {
+  console.log(req.body);
+
+  const { name, email, password } = req.body;
+
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    generateToken(res, user._id);
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
+  //   res.status(200).json({ message: "Register User" });
+});
+// @desc logout
+// route POST /api/users/logout
+// access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+// @desc get user profile
+// route GET /api/users/profile
+// access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile" });
+});
+// @desc update user profile
+// route PUT /api/users/profile
+// access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User profile update" });
+});
+
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
---
+++ backend/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dontenv from "dotenv";
+dontenv.config();
+// error middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import connectDB from "./config/db.js";
+const port = process.env.PORT || 5000;
+import userRputes from "./routes/userRoutes.js";
+
+connectDB();
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/users", userRputes);
+app.get("/", (req, res) => res.send("server is ready"));
+app.use(notFound);
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
