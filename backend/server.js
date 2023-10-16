b21ad0c1f332355342328e46bc3709b5a62d568d
Md Fahad kabir <fahadkabirsezan@gmail.com>
2023-10-16 16:56:53 +0600

    frontend and backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dontenv from "dotenv";
 dontenv.config();
+import cookieParser from "cookie-parser";
 // error middleware
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
@@ -12,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+app.use(cookieParser())
 app.use("/api/users", userRputes);
 app.get("/", (req, res) => res.send("server is ready"));
 app.use(notFound);
