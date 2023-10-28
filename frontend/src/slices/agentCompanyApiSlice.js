026f9ccc6ca1d93fb912dec44cf290aef7c22e08
FahadKabirKsl <fahadkabirsezan@gmail.com>
2023-10-28 23:11:18 +0600

    axios

--- frontend/src/slices/agentCompanyApiSlice.js
+++ frontend/src/slices/agentCompanyApiSlice.js
@@ -1,23 +1,45 @@
+// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+// const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000/api" });
+
+// export const agentCompanyApiSlice = createApi({
+//   reducerPath: "agentCompanyApi",
+//   baseQuery,
+//   endpoints: (builder) => ({
+//     addAgent: builder.mutation({
+//       query: (agentData) => ({
+//         url: "/agents/add-agent",
+//         method: "POST",
+//         body: agentData,
+//       }),
+//     }),
+//     getMyAgents: builder.query({
+//       query: () => "/agents/myagents",
+//     }),
+//   }),
+// });
+
+// export const { useAddAgentMutation, useGetMyAgentsQuery } =
+//   agentCompanyApiSlice;
+
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:5000/api" });
-
-export const agentCompanyApiSlice = createApi({
+export const agentCompanyApi = createApi({
   reducerPath: "agentCompanyApi",
-  baseQuery,
+  baseQuery: fetchBaseQuery({ baseUrl: "/api/agents" }),
   endpoints: (builder) => ({
     addAgent: builder.mutation({
       query: (agentData) => ({
-        url: "/agents/add-agent",
+        url: "/add-agent",
         method: "POST",
         body: agentData,
       }),
     }),
     getMyAgents: builder.query({
-      query: () => "/agents/myagents",
+      query: () => "/myagents",
     }),
   }),
 });
 
-export const { useAddAgentMutation, useGetMyAgentsQuery } =
-  agentCompanyApiSlice;
+export const { useAddAgentMutation, useGetMyAgentsQuery } = agentCompanyApi;
+
